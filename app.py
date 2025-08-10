86720a407b604b873a357d31a87a43e64fe18d53
haidarmaula <haidarmaulareynaldy@gmail.com>
2025-08-10 15:56:12 +0700

    chore: update port configuration

--- app.py
+++ app.py
@@ -196,4 +196,5 @@ if __name__ == "__main__":
     """
     Runs the Flask server on localhost at port 5000.
     """
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
